c0a76bf3500bde9de445c976cb2f2302346bb144
nmieza <nurmieza14@gmail.com>
2022-04-21 10:41:14 +0800

    commiy

--- variable.php
+++ variable.php
@@ -10,5 +10,12 @@
     <?php 
     $username = "Fred Smith"
     echo $username;
+    echo "<br />";
+    $current_user = $username;
+    echo $current_user;
+    echo "<br />";
+    $num1 = 10.5;
+    echo $num1;
+    ?>
 </body>
 </html>
