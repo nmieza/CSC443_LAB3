a3cd7ac5da62dce9e45d82521ebb3b6f6e9c6d7a
nmieza <nurmieza14@gmail.com>
2022-04-21 23:07:13 +0800

    commit

--- string2.php
+++ string2.php
@@ -15,8 +15,8 @@
 
     $num1 = 11;
     $num2 = 15;
-    $sum = add($num1. $num2);
-    echo "$num1 + $num2 = " . $sum;
+    $summ = add($num1. $num2);
+    echo "$num1 + $num2 = " . $summ;
     ?>
 </body>
 </html>
