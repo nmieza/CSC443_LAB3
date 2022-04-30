52d406e6d58b655512893845aed885c3742ffe74
nmieza <nurmieza14@gmail.com>
2022-04-30 17:19:51 +0800

    commit

--- comment.php
+++ comment.php
@@ -9,7 +9,7 @@
 <body>
     <?php 
     // This is a comment
-    // echo "X equals $x";
+    // echo "X equals $x"
     $x += 10; // Increment $x by 10
     echo $x;
     /* This is a section of multiline comments that will not be interpreted */
